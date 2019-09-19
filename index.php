e94e93fa08e722ec7eb8428a47ab0d63a376f7ed
papesaer <papepolgamb@gmail.com>
2019-09-19 14:03:06 +0200

    index

---
+++ index.php
@@ -0,0 +1,147 @@
+<?php
+$home = "index.php";
+?>
+<!doctype html>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
+<head>
+	<style>
+
+p {
+	color: red;
+}
+</style>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="assets/css/bootstrap.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/style.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <title>Gestion produit</title>
+</head>
+<body>
+    <div class="container" style="margin-top: 50px;
+        margin-bottom: 100px;">
+        <nav>
+    <ul class="nav nav-tabs nav-justified navbar-fixed-top">
+        <li>
+            <a class="btn" style="color: lime; background-color: blue; height: -40px;
+  width: -100px;" href="<?= $home ?>">
+                <h2>ACCUEIL</h2>
+            </a>
+        </li>
+        
+         <li>
+            <a class="btn" href="client.php">
+                <h4>Formulaires des employes</h4>
+            </a>
+        </li>
+        
+        </li>
+       
+       
+       
+         <li>
+            <a class="btn" href="listclient.php">
+                <h4>listes des employes</h4>
+            </a>
+        </li>
+        
+        
+    </ul>
+</nav><br><br><br><br>
+        
+        
+        
+
+        <div class="col-md-6 col-md-offset-3">
+           
+        
+
+
+        </div><marquee> <h1>SEUL LE TRAVAIL PAYE CHEZ NOUS !</h1></marquee><br><br>
+                                                 <div class="container">
+  <h2>L ENTREPRISE C EST VOUS</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="m1.jpeg" alt="Los Angelesmxzsmxnz" style="width:20%;">
+      </div>
+
+      <div class="item">
+        <img src="m7.jpeg" alt="Chicago hgsdsghdsga" style="width:20%;">
+      </div>
+    
+      <div class="item">
+        <img src="m3.jpeg" alt="New york xhjasxdaysjxass" style="width:20%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+        <p class="flotte">
+ <img src="vente.jpg" alt="Nouveau !"  class="img-fluid"/>
+</p>
+<p style="line-height:65px;">
+ L esprit d equipe est nos credos.
+</p>  
+<hr>
+        
+                   <img src="pape1.jpeg" class="fill";/>Departement informatique<img src="pape2.jpeg"  class="fill";/>Departement Logistique<img src="pape3.jpeg"  class="fill";/>Departemen comunication<img src="pape4.jpeg"  class="fill";/><br/><br><br>
+
+                   
+                   
+
+
+
+                   
+<div class="alert alert-primary" role="alert">
+  JE N ABANDONNE JAMAIS MON EQUIPE CAR C MA SECONDE FAMILLE  <a href="#" class="alert-link"> un example link</a>. Cliquer pour aimer.
+</div>
+
+
+
+
+
+                   <hr>
+
+ 
+    </div>
+
+
+
+
+</body>
+</html>
